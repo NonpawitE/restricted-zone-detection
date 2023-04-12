0de657dff34cff3b17fec97fb74cd422a0ac75ee
Nonpawit Ekburanawat <nonpawit.ek@gmail.com>
2023-04-12 20:28:49 +0700

    Add video canvas and video control button

--- user_interface.py
+++ user_interface.py
@@ -8,18 +8,23 @@ Created on Tue Apr 11 2023
 @author: Nonpawit Ekburanawat
 """
 
-import tkinter as tk
-import cv2 as cv
-from PIL import Image, ImageTk
+import tkinter as     tk
+import cv2     as     cv
+from   PIL     import Image, ImageTk
+from   tkinter import filedialog
 
 # Video Capture Class Handler
 
 class VideoStream:
-    def __init__(self, src=0, master=None):
-        self.stream                 = cv.VideoCapture(src)
+    def __init__(self, src=None, master=None):
+        self.stream = cv.VideoCapture(src) if src else cv.VideoCapture(0)
         
-        self.stream.set(cv.CAP_PROP_FRAME_WIDTH,  640)
-        self.stream.set(cv.CAP_PROP_FRAME_HEIGHT, 480)
+        self.stream.set(cv.CAP_PROP_FRAME_WIDTH,  1280)
+        self.stream.set(cv.CAP_PROP_FRAME_HEIGHT, 720)
+        self.stream.set(cv.CAP_PROP_AUTOFOCUS,    1)
+        self.stream.set(cv.CAP_PROP_AUTO_WB,      1)
+        self.stream.set(cv.CAP_PROP_FPS,          30)
+
         
         (self.grabbed, self.frame)  = self.stream.read()
         self.stopped                = False
@@ -29,41 +34,76 @@ class VideoStream:
         self.master.after(0, self.update)
         
     def update(self):
-        (self.grabbed, self.frame)  = self.stream.read()
-        self.frame = cv.cvtColor(self.frame, cv.COLOR_BGR2RGB)
-        
-        self.photo = ImageTk.PhotoImage(image=Image.fromarray(self.frame))
-        canvas.create_image(0, 0, image=self.photo, anchor=tk.NW)
-        
         if not self.stopped:
-            self.master.after(10, self.update)
+            (self.grabbed, self.frame) = self.stream.read()
+            if self.grabbed:
+                self.frame = cv.cvtColor(self.frame, cv.COLOR_BGR2RGB)
+                self.photo = ImageTk.PhotoImage(image=Image.fromarray(self.frame))
+                canvas.create_image(0, 0, image=self.photo, anchor=tk.NW)
+                self.master.after(10, self.update)
+            else:
+                self.stop()
+                canvas.delete("all")
         
     def stop(self):
         self.stopped = True
+        self.stream.release()
 
-def on_close():
-    vs.stop()
-    vs.stream.release()
-    root.destroy()
+
+# Button Functions
+
+def stop_camera():
+    global vs
+    if vs:
+        vs.stop()
+        canvas.delete("all")
+    
+def start_camera():
+    global vs
+    stop_camera()
+    vs = VideoStream(master=root)
+    vs.start()
+
+
+def open_file():
+    file_path   = filedialog.askopenfilename()
+    global vs
+    stop_camera()
+    vs          = VideoStream(file_path, master=root)
+    vs.start()
+
 
 # Initialize Window Settting
 
-root = tk.Tk()
-root.geometry("1000x700")
+root = tk.Tk()  
+root.geometry("1400x850")
 root.title("Restricted Zone Detection")
 root.resizable(False, False)
 
+
 # Create Canvas for Video Capture
 
-canvas = tk.Canvas(root, width=640, height=480)
+canvas = tk.Canvas(root, width=1280, height=720)
 canvas.pack()
 
-# Initialize Video Capture
 
-vs = VideoStream(master=root)
-vs.start()
+# Video Buttons
 
-# Start Main Loop
+start_button = tk.Button(root, text="Start Camera",    command=start_camera)
+start_button.pack(side=tk.LEFT, padx=10, pady=10)
 
-root.protocol("WM_DELETE_WINDOW", on_close)
+stop_button  = tk.Button(root, text="Stop Camera",     command=stop_camera)
+stop_button.pack(side=tk.LEFT, padx=10, pady=10)
+
+open_button  = tk.Button(root, text="Open Video File", command=open_file)
+open_button.pack(side=tk.LEFT, padx=10, pady=10)
+
+
+# Main Loop & Callback
+
+def on_closing():
+    stop_camera()
+    root.destroy()
+    
+root.protocol("WM_DELETE_WINDOW", on_closing)
 root.mainloop()
