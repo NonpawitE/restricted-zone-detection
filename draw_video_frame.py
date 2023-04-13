9ace7c4af5386f950ac9090838d042b8c20a7205
Nonpawit Ekburanawat <nonpawit.ek@gmail.com>
2023-04-13 18:42:13 +0700

    Update draw_video_frame.py
    Be able to draw a frame in video

--- draw_video_frame.py
+++ draw_video_frame.py
@@ -7,3 +7,72 @@ Created on Wed Apr 5 2023
 
 @author: Nonpawit Ekburanawat
 """
+
+import cv2 as cv
+import numpy as np
+
+rectangles = []
+drawing = False
+ix, iy = -1, -1
+
+def draw_rectangle(event, x, y, flags, param):
+    global ix, iy, drawing, img, rectangles
+
+    if event == cv.EVENT_LBUTTONDOWN:
+        drawing = True
+        ix, iy = x, y
+
+    elif event == cv.EVENT_MOUSEMOVE:
+        if drawing:
+            img_copy = img.copy()
+            if len(rectangles) > 0:
+                # Remove the last rectangle from the list
+                rectangles.pop()
+            cv.rectangle(img_copy, (ix, iy), (x, y), (0, 255, 0), 1)
+            cv.imshow('image', img_copy)
+
+    elif event == cv.EVENT_LBUTTONUP:
+        drawing = False
+        rectangles.append(((ix, iy), (x, y)))
+
+
+# Open the default camera
+cap = cv.VideoCapture(0)
+
+# Check if camera is opened successfully
+if not cap.isOpened():
+    print("Error opening video stream or file")
+
+# Read the first frame
+_, frame = cap.read()
+
+# Make a copy of the first frame for drawing
+img = np.copy(frame)
+
+# Create a window and set the mouse callback
+cv.namedWindow('image')
+cv.setMouseCallback('image', draw_rectangle)
+
+while True:
+    # Draw all the rectangles in the list
+    img_copy = img.copy()
+    for r in rectangles:
+        cv.rectangle(img_copy, r[0], r[1], (0, 255, 0), 1)
+    cv.imshow('image', img_copy)
+
+    # Wait for key input
+    key = cv.waitKey(1) & 0xFF
+
+    # If 'q' is pressed, break the loop
+    if key == ord('q'):
+        break
+
+    # Read the next frame from the camera
+    _, frame = cap.read()
+
+    # Make a copy of the frame for drawing
+    img = np.copy(frame)
+
+# Release the camera capture and destroy all windows
+cap.release()
+cv.destroyAllWindows()
